b54463ffbdeee95db29c8a1d053f1551d5fef03a
Lavinia110 <55039893+Lavinia110@users.noreply.github.com>
2019-09-19 13:16:02 +0800

    Add files via upload

---
+++ 年.cpp
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std; 
+int main()
+{
+	int year,month,day,T=0,week;//定义输入变量；
+	int  leapyear[12] = {31,29,31,30,31,30,31,31,30,31,30,31};//闰年每月天数；
+	int commonyear[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };//平年每月天数；
+	
+	cout << "输入年月日：" << endl;
+	cin >> year>> month>> day;
+	int m = month - 1;
+	cout << year<< "-" << month << "-" << day << endl;//显示输入的日期；
+	if ((month <= 12) && (month >= 1)) //验证输入日期是否正确；
+	{
+		if ((year % 400 == 0) || (year % 4 == 0) && (year % 100 != 0))//如果是闰年则运行；
+		{
+			if (day <= leapyear[m])//且输入的日期不超过天数正常值；
+			{
+				for (int i = 0; i < month; i++)
+					T = T + leapyear[i];
+				if (T % 7 != 0)
+					week = T / 7 + 1;
+				else
+					week = T / 7;
+				cout << "第" << T << "天" << "周" << week << endl;
+			}
+			else cout << "输入日期不正确" << endl;
+		}
+		else
+		{
+			if (day <= commonyear[m])
+			{
+				for (int j = 0; j < month; j++)
+					T = T + leapyear[j];
+				if (T % 7 != 0)
+					week = T / 7 + 1;
+				else
+					week = T / 7;
+				cout << "第" << T << "天" << week << "周" << endl;
+			}
+			else cout << "输入日期不正确" << endl;
+		}
+	//else cout << "输入日期不正确" << endl;	
+	}
+	system("pause");
+	return 0;
+}
